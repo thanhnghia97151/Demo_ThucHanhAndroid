2791014960f94e990d7ab38cc9f3aa7b7902f6b1
unknown <nguyenthanhnghia0907@gmail.com>
2021-05-28 14:44:48 +0700

    Bai 1

---
+++ app/src/main/java/com/example/thithuandroid/ItemAPI.java
@@ -0,0 +1,18 @@
+package com.example.thithuandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ItemAPI extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_api);
+    }
+
+
+}
